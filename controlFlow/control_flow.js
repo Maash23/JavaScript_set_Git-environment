cff4699cc43b5924209a8b5b2e1b931116cf396e
Maash23 <ahmedmaash.official@gmail.com>
2025-11-13 05:52:22 -0500

    JS control flow_conditional statements

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -41,3 +41,9 @@ switch (userType) {
 }
 
 console.log("User Category;" , userCategory);
+
+let isAuthenticated = true;
+
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+
+console.log("Authentication Status:", authenticationStatus)
